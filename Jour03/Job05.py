cfa38c09ca349dd2f934493d4d45063928673f7d
CoinFLip2 <pierrelouis3131@gmail.com>
2024-01-08 10:06:56 +0100

    Job05

--- Jour03/Job05.py
+++ Jour03/Job05.py
@@ -18,3 +18,56 @@ class Personnage:
         degats = random.randint(0,20)
         print(f"{self._nom} a attaqué et à fait {degats} points de degats")
         ennemi.predreDegats(degats)
+
+    
+class Jeu:
+    def __init__(self):
+        self._niveau = None
+        self._joueur = None
+        self._ennemi = None
+    
+    def choisirNiveau(self):
+        self._niveau = int(input("Choisissez le niveau de difficulté (1, 2, 3): "))
+        if self._niveau == 1:
+            vie = 100
+            vie_ennemi = 80
+        elif self._niveau == 2:
+            vie = 60
+            vie_ennemi = 60
+        else:
+            vie = 30
+            vie_ennemi = 40
+        return vie, vie_ennemi
+
+    def lancerJeu(self):
+        vie, vie_ennemi = self.choisirNiveau()
+        self._joueur = Personnage("Joueur", vie)
+        self._ennemi = Personnage("Ennemi", vie_ennemi)
+        self.jouer()
+
+    def verifierSante(self):
+        if not self._joueur._estVivant:  
+            return False
+        elif not self._ennemi._estVivant:
+            return False
+        return True
+    
+    def verifierGagnant(self):
+        if not self._joueur._estVivant:
+            print("Le joueur est mort")
+        elif not self._ennemi._estVivant:
+            print("L'ennemi est mort")
+
+    def jouerTour(self):
+        self._joueur.attaquer(self._ennemi)
+        if self.verifierSante():
+            self._ennemi.attaquer(self._joueur)
+        self.verifierSante()
+    
+    def jouer(self):
+        while self.verifierSante():
+            self.jouerTour()
+        self.verifierGagnant()
+        
+jeu = Jeu()
+jeu.lancerJeu()
